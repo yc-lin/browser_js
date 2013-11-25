d3857b35b0f8fa8bf13fa9667d14907e77452081
yclin <linlichangjen@gmail.com>
2013-11-25 11:04:54 +0800

    U: update bookmark.js URL

--- bookmark.js
+++ bookmark.js
@@ -1,2 +1,4 @@
+// please add this javascript code into bookmark
 
-javascript:(function(){var s=document.getElementById("tongwenlet_tw");if(s!=null){document.body.removeChild(s);}var s=document.createElement("script");s.language="javascript";s.type="text/javascript";s.src="http://tongwen.openfoundry.org/src/bookmarklet/convert_cn_tw.js";s.id="cn_tw";document.body.appendChild(s); })();
+javascript:(function(){var s=document.getElementById("tongwenlet_tw");if(s!=null){document.body.removeChild(s);}var s=document.createElement("script");s.language="javascript";s.type="text/javascript";s.src="https://raw.github.com/yc-lin/browser_js/master/convert_cn_tw.js";s.id="tongwenlet_tw";document.body.appendChild(s); })();
+
